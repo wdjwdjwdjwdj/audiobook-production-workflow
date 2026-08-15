9c88aa876ff5ebb0abce376281d0c03afaf829c6
爻 <2968874474@qq.com>
2026-08-16 07:07:18 +0800

    feat: implement audiobook review MVP

---
+++ audiobook/domain/__init__.py
@@ -0,0 +1,15 @@
+from .models import (
+    AlignmentItem,
+    ReviewIssue,
+    ScriptSegment,
+    Transcript,
+    TranscriptSegment,
+)
+
+__all__ = [
+    "AlignmentItem",
+    "ReviewIssue",
+    "ScriptSegment",
+    "Transcript",
+    "TranscriptSegment",
+]
